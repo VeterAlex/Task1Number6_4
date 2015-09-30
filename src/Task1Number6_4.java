f7e0a7ef1a49be28439db5381bf39246de1bbbc6
VeterAlex <twinzata@gmail.com>
2015-09-30 11:27:27 +0300

    Initial commit

---
+++ src/Task1Number6_4.java
@@ -0,0 +1,13 @@
+
+public class Task1Number6_4 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                if (i % 7 ==0)
+                System.out.print(i);
+                System.out.print(" ");
+            }
+        }
+    }
+}
